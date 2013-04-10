05ffda12532e23962ffec5213868728efa447deb
Avduevskiy V <gloreus@gmail.com>
2013-04-10 22:47:48 +0400

    подгрузка шаблонов

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+# -*- coding: utf-8 -*- 
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
-simple_page = Blueprint('simple_page', __name__,
-                        template_folder='templates')
+static_page = Blueprint('static_page', __name__)
 
-@simple_page.route('/', defaults={'page': 'index'})
-@simple_page.route('/<page>')
-def show_page(page):
+@static_page.route('/st_content/<page_name>')
+def show(page_name):
     try:
-        return render_template('pages/%s.html' % page)
+        return render_template('base.html', content_name = 'content/' + page_name + '.html')
     except TemplateNotFound:
-        abort(404)
+		return page_name
+        # abort(404)
--- tdodin.py
+++ tdodin.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*- 
 
 from flask import Flask
-from main import simple_page
+from main import static_page
 
 app = Flask(__name__)
 app.debug = True
-app.register_blueprint(simple_page)
+
+app.register_blueprint(static_page) # /st_content/*
