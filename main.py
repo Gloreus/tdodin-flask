007030858ea3a559d8532cb22966f0c331e64b41
Avduevskiy V <gloreus@gmail.com>
2013-04-11 23:10:20 +0400

    настрока шаблонов, главной страницы

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 # -*- coding: utf-8 -*- 
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
+import data
 
 static_page = Blueprint('static_page', __name__)
 
+@static_page.route('/')
 @static_page.route('/st_content/<page_name>')
-def show(page_name):
+def show(page_name = None):
     try:
-        return render_template('base.html', content_name = 'content/' + page_name + '.html')
+		if not page_name:
+			return render_template('base.html', content_name = 'content/tdodin.html')
+		else:
+			return render_template('base.html', content_name = 'content/' + page_name + '.html')
+		
     except TemplateNotFound:
 		return page_name
         # abort(404)
+
+		
+cat_page = Blueprint('cat_page', __name__)
+
+@cat_page.route('/<page>')
+def show(page):
+	s = u''
+	for r in data.GetTree():
+		s += r['name']
+	return s	
--- tdodin.py
+++ tdodin.py
@@ -2,8 +2,12 @@
 
 from flask import Flask
 from main import static_page
+from main import cat_page
+
+
 
 app = Flask(__name__)
 app.debug = True
 
 app.register_blueprint(static_page) # /st_content/*
+app.register_blueprint(cat_page) # /
